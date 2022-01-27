c58f782cca94c6f9bc448f7b63fbe32d16dbf5af
Your Name <you@example.com>
2022-01-27 20:00:51 +0300

    #1 first

--- teacher/4/start.py
+++ teacher/4/start.py
@@ -1,2 +1,7 @@
 print("Hello Dima")
-number = input("Enter number 1")
+number1 = int(input("Enter number 1"))
+number2 = int(input("Enter number 2"))
+operation = input("Operation (+-/*)")
+
+if operation == '+':
+    print(number1+number2)
