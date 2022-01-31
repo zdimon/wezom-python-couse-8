c2e6cb2cde76eaaa4f39a934fdb7221a08f64ce0
Nina <ninakzv@gmail.com>
2022-01-31 13:37:22 +0200

    calc4

--- students/Nina/calc.py
+++ students/Nina/calc.py
@@ -1,6 +1,5 @@
 stroka = '2+5+7+12'
 list_s = stroka.split('+')
 for num in range(1,4):
-    sum = int(list_s[num])+int(list[num+1])
-
+    sum = int([list_s[num]])+int([list[num+1]])
 print (sum)
--- students/Nina/calc4.py
+++ students/Nina/calc4.py
@@ -1,4 +1,4 @@
 stroka = '4 + 5 * 2 - 3'
-list_s =stroka.split(' ')
-itog = int(list_s[3]) * int(list_s[5]) + int(list_s[1]) - int(list_s[7])
-print (itog)
+spisok = stroka.split(' ')
+itogo = int(spisok[2]) * int(spisok[4]) + int(spisok[0]) - int(spisok[6])
+print (itogo)
