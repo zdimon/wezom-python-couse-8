e8798c8c0ea81d2cf89fc2046cc795a85cacbe65
Nina <ninakzv@gmail.com>
2022-01-31 18:03:33 +0200

    www

--- students/Nina/calc2.py
+++ students/Nina/calc2.py
@@ -1,13 +1,18 @@
-n1 = int(input("Enter first number:"))
-n2 = int(input("Enter second number:"))
-oper = input("Operations(+/-/*/:")
-
-if oper == '+' :
-    print (n1+n2)
-elif oper == '-' :
-    print (n1-n2)
-elif oper == '*' :
-    print (n1*n2)
-elif oper == '/' :
-    print (n1/n2)
-else : print ("ERROR")
+indicator = 'y'
+while indicator == 'y' :
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    oper = input("Operations(+/-/*/: ") 
+    if oper == '+' :
+        print (n1+n2)
+    elif oper == '-' :
+        print (n1-n2)
+    elif oper == '*' :
+        print (n1*n2)
+    elif oper == '/' :
+        print (n1/n2)
+    else : 
+        print ("ERROR")
+    indicator = input("продолжаем? y/n: ")
+else :
+    print("Игра закончена")
