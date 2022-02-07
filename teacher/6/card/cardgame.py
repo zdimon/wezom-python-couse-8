01963eedbf8985cb61ad8cc18e8d8f4115b0ab11
Your Name <you@example.com>
2022-02-07 20:19:39 +0300

    commit from zdimon77@gmail.com ----

--- teacher/6/card/cardgame.py
+++ teacher/6/card/cardgame.py
@@ -14,14 +14,22 @@ print(deck)
 '''
 print('Start game')
 faces = ['diamond', 'club', 'heart', 'spade']
-cards = ['6','7','8']
+cards = ['2','3','4','5','6','7','8','9','10']
 deck = []
 for face in faces:
     for card in cards:
-        print(card+'-'+face)
+        #print(card+'-'+face)
         deck.append(card+'-'+face)
 
 
-print(deck)
+#print(deck)
 random.shuffle(deck)
-print(deck)
+hand = deck[0:4]
+account = 0
+for card in hand:
+    print(card)
+    tmp = card.split('-')
+    print(tmp[0])
+    account = account + int(tmp[0])
+
+print(account)
