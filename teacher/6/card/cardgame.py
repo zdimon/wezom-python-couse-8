68db2a5e0b27e64c26ce414fcb93c46e7712fe49
Your Name <you@example.com>
2022-02-07 20:05:12 +0300

    commit from zdimon77@gmail.com ----

--- teacher/6/card/cardgame.py
+++ teacher/6/card/cardgame.py
@@ -1,27 +1,23 @@
 '''
 на нужно получить список карт
-
-fases = ['diamond', 'club', 'heart', spade]
-
-cards = ['6','7'....]
-
+fases = ['diamond', 'club', 'heart', 'spade']
+cards = ['2','6','7'....]
 deck = ['diamond-6','club-q','heart-a','spade-k', 'spade-j']
-
 deck = []
-
-
 for face in fases:
     print(face)
     for card in cards:
         print(card)
         deck.append(card) 
-
 print(deck)
-
-
-
 '''
 print('Start game')
 faces = ['diamond', 'club', 'heart', 'spade']
-print(faces)
+cards = ['6','7','8']
+deck = []
+for face in faces:
+    for card in cards:
+        print(card+'-'+face)
+        deck.append(card+'-'+face)
 
+print(deck)
