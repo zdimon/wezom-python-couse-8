f3ef3d71f5877c020d23bb8c4a568f113feb730e
Your Name <you@example.com>
2022-02-10 18:58:23 +0200

    com2

--- students/Vitalii/7/vitaly_calc.py
+++ students/Vitalii/7/vitaly_calc.py
@@ -1,7 +1,17 @@
 def calc(spisok: list,koef: int):
     print(spisok)
-
+    tmp = []
     for number in spisok:
-        print(number*koef)
+        sum = number*koef
+
+        tmp.append(sum)
+    print(tmp)
+
+    res = 0
+    for num in tmp:
+        res= num + res
+
+    print (res)
 
 calc([3,2,5,4,],5)    
+
