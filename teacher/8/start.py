e36679fb44c37fe8a07df62153a3f0ce7d7d6349
Your Name <you@example.com>
2022-02-14 19:54:37 +0300

    commit from zdimon77@gmail.com ----

--- teacher/8/start.py
+++ teacher/8/start.py
@@ -1,6 +1,15 @@
 import requests
 import sys
+from bs4 import BeautifulSoup
+    
 # python3 -m venv venv
-result = requests.get('https://www.arduinka.top')
-print(result.text)
-print(dir(result))
+# pip install -r requirements.txt
+result = requests.get('https://pythondigest.ru/')
+#print(result.text)
+soup = BeautifulSoup(result.text, 'html.parser')
+links = soup.findAll('a')
+# bd = [{"title": "about", "href": "url"},{},{}]
+bd = []
+for link in links:
+    bd.append({"title": link.text, "url": link.get('href')})
+print(bd)
