a8ac0e0321d1b21ce86c6a0a8a90746c4a207656
Your Name <you@example.com>
2022-02-17 20:18:51 +0300

    commit from zdimon77@gmail.com ----

--- shop/shop/shop/settings.py
+++ shop/shop/shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-im$@)7hprwq#w00lyuo1b98@zw$b54v)pa&@ufgn&3==a8gr3l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
